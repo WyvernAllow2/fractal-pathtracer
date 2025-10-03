729c93bcf1cc88cb121223f90d8844326daefe24
WyvernAllow <bloodflame2535@gmail.com>
2025-10-03 21:05:04 +0200

    Window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,55 @@
 #include <iostream>
 
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/gl.h>
+
+static void glfw_error_callback(int error_code, const char* description) {
+	std::cerr << "GLFW: " << description << "\n";
+}
+
 int main() {
-	std::cout << "Hello, World!\n";
+	if (!glfwInit()) {
+		std::cerr << "glfwInit() failed\n";
+		return EXIT_FAILURE;
+	}
+
+	GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+	const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+	glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+	glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+	glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+	glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+	GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "Quadcraft", NULL, NULL);
+	if (!window) {
+		std::cerr << "glfwCreateWindow() failed\n";
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGL(glfwGetProcAddress)) {
+		std::cerr << "gladLoadGL() failed\n";
+		return EXIT_FAILURE;
+	}
+
+	float current_time = (float)glfwGetTime();
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		float new_time = (float)glfwGetTime();
+		float delta_time = new_time - current_time;
+		current_time = new_time;
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 }
